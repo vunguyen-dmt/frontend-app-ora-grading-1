a4533c679404767ee655c5eaf1fa838770d0704d
luongdmttek <luong.ngo@dmttek.com>
2023-05-04 17:28:53 +0700

    vi language

--- src/i18n/index.jsx
+++ src/i18n/index.jsx
@@ -3,12 +3,14 @@ import arMessages from './messages/ar.json';
 import es419Messages from './messages/es_419.json';
 import frMessages from './messages/fr.json';
 import zhcnMessages from './messages/zh_CN.json';
+import viMessages from './messages/vi.json';
 
 const messages = {
   ar: arMessages,
   'es-419': es419Messages,
   fr: frMessages,
   'zh-cn': zhcnMessages,
+  vi: viMessages,
 };
 
 export default messages;
